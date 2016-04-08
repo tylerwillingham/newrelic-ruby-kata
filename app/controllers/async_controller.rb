6c0acadc7a5dddc5d57d4f1a63cf32ea3f1a6fec
Tyler Willingham <tylerwillingham@gmail.com>
2016-04-07 20:55:39 -0500

    Don't trigger emails synchronously... especially from a view

--- app/controllers/async_controller.rb
+++ app/controllers/async_controller.rb
@@ -1,4 +1,8 @@
 class AsyncController < ApplicationController
   def index
+    # We're using the inline adapter in this app it appears...
+    # but ideally you would be doing this via Sidekiq or another
+    # queue where we can actually do this async
+    SendLotteryTicketToUserJob.perform_later
   end
 end
